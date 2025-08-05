c5dd30d3579b023ac2626b9c23a2761dedc2dc71
zain anis <zainanis28862@gmail.com>
2025-08-05 17:40:34 +0500

    Set the routes connecting expenses to the main Router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const productsRouter = require("./product.routes");
 const customerRouter = require("./customer.routes");
 const orderRouter = require("./orders.routes");
+const expenseRouter = require("./expenses.routes");
 const router = express.Router();
 
 router.use("/products", productsRouter);
 router.use("/customers", customerRouter);
 router.use("/orders", orderRouter);
-
+router.use("/expenses", expenseRouter);
 module.exports = router;
