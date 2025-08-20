76a0b5bb5a419171691c952e0449eef6bfecffa5
zain anis <zainanis28862@gmail.com>
2025-08-20 23:18:07 +0500

    creating the ledger tab

--- frontend/src/components/utils/Mytable.jsx
+++ frontend/src/components/utils/Mytable.jsx
@@ -5,7 +5,13 @@ import { useNavigate } from "react-router"; // If using react-router
 
 import Modal from "./Modal";
 
-const Mytable = ({ who = "items", data = [], header = [], filter = "All" }) => {
+const Mytable = ({
+  who = "items",
+  data = [],
+  header = [],
+  filter = "All",
+  orderBy = null,
+}) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [selectedItem, setSelecteditem] = useState(null);
@@ -16,6 +22,31 @@ const Mytable = ({ who = "items", data = [], header = [], filter = "All" }) => {
       : data.filter((item) => {
           return item.type === filter;
         });
+  if (orderBy) {
+    filteredData.sort((a, b) => {
+      const aValue = new Date(a[orderBy]);
+      const bValue = new Date(b[orderBy]);
+
+      // return aValue - bValue;
+      return bValue - aValue;
+    });
+  }
+  let runningTotal = 0;
+
+  const dataToRender =
+    who === "ledger"
+      ? filteredData.map((data) => {
+          if (data.type === "Profit") {
+            runningTotal += Number(data.amount);
+          } else {
+            runningTotal -= Number(data.amount);
+          }
+          return {
+            ...data,
+            totalAmount: runningTotal.toFixed(2),
+          };
+        })
+      : filteredData;
 
   const getValueFromPath = (obj, path) => {
     if (!path) return "";
@@ -60,15 +91,19 @@ const Mytable = ({ who = "items", data = [], header = [], filter = "All" }) => {
               </th>
             ))}
 
-            <th className="px-2 py-3 text-xs text-pink-900 font-bold uppercase ">
-              Options
-            </th>
+            {who === "ledger" ? (
+              ""
+            ) : (
+              <th className="px-2 py-3 text-xs text-pink-900 font-bold uppercase ">
+                Options
+              </th>
+            )}
           </tr>
         </thead>
 
         <tbody className="bg-white divide-y divide-pink-100">
-          {filteredData.length > 0 ? (
-            filteredData.map((item) => (
+          {dataToRender.length > 0 ? (
+            dataToRender.map((item) => (
               <tr
                 key={item._id}
                 className={
@@ -89,29 +124,33 @@ const Mytable = ({ who = "items", data = [], header = [], filter = "All" }) => {
                     {getValueFromPath(item, head.path)}
                   </td>
                 ))}
-                <td className="px-2 py-4   text-center flex gap-2 justify-center">
-                  <button
-                    className=" bg-pink-800 text-white px-6 py-2 rounded-lg hover:bg-pink-900"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleEdit(item._id);
-                    }}
-                  >
-                    Update
-                  </button>
-                  <button
-                    className=" bg-pink-800 text-white px-6 py-2 rounded-lg hover:bg-pink-900"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSelecteditem({
-                        id: item._id,
-                        name: item.name || item.customerId.name,
-                      });
-                    }}
-                  >
-                    Delete
-                  </button>
-                </td>
+                {who === "ledger" ? (
+                  ""
+                ) : (
+                  <td className="px-2 py-4   text-center flex gap-2 justify-center">
+                    <button
+                      className=" bg-pink-800 text-white px-6 py-2 rounded-lg hover:bg-pink-900"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleEdit(item._id);
+                      }}
+                    >
+                      Update
+                    </button>
+                    <button
+                      className=" bg-pink-800 text-white px-6 py-2 rounded-lg hover:bg-pink-900"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setSelecteditem({
+                          id: item._id,
+                          name: item.name || item.customerId.name,
+                        });
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                )}
               </tr>
             ))
           ) : (
--- frontend/src/pages/Ledger.jsx
+++ frontend/src/pages/Ledger.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { FaPlus } from "react-icons/fa";
+import { NavLink } from "react-router";
+import Mytable from "../components/utils/Mytable.jsx";
+
+import { useDispatch, useSelector } from "react-redux";
+import { setExpenses } from "../state/expenseSlice.js";
 
 const Ledger = () => {
-  return <div>Ledger</div>;
+  const dispatch = useDispatch();
+  const [ledger, setLedger] = useState([]);
+
+  const [filter, setFilter] = useState("All");
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5001/api/ledger")
+      .then((res) => {
+        setLedger(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className=" bg-white rounded-lg shadow flex gap-2 p-2  flex-col justify-between  ">
+      <div className="flex flex-col gap-5">
+        <div className="border-solid border-b-2 pt-15 px-5 border-stone-200 flex justify-between flex-wrap py-5">
+          <h1 className="font-bold text-4xl text-pink-900">Expenses</h1>
+          <input
+            type="text"
+            className="border-1 border-solid border-stone-200 rounded-lg pr-20 py-2 pl-5"
+            placeholder="Search Expenses"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+
+        <div className="flex justify-end px-5 gap-5 ">
+          <NavLink
+            className=" flex items-center justify-center gap-2 px-4 py-2 rounded-2xl bg-green-600 text-white hover:bg-green-700 hover:shadow-lg"
+            to="add"
+          >
+            <FaPlus />
+            Add Expense
+          </NavLink>
+
+          <select
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+            className="border px-4 py-2 rounded border-stone-200 text-stone-500"
+          >
+            <option value="All">All</option>
+            <option value="Goods Purchase">Goods Purchase</option>
+            <option value="Advertisement">Advertisement</option>
+          </select>
+        </div>
+        <Mytable
+          who="ledger"
+          data={ledger}
+          header={[
+            { label: "Source", path: ".source" },
+            { label: "Payment Mode", path: ".paymentMode" },
+            { label: "type", path: ".type" },
+            { label: "Amount", path: ".amount" },
+            { label: "Date", path: ".date" },
+            { label: "Total Amount", path: ".totalAmount" },
+          ]}
+          filter={filter}
+          orderBy="date"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Ledger;
